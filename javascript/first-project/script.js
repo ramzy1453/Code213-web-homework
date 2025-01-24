b26e785b172416efce6d625e81bb6cfb1e005e44
ramzy1453 <kemmounramzy93@gmail.com>
2025-01-24 14:55:31 +0100

    add first projet of js (basics : variable, data types, console.log)

---
+++ javascript/first-project/script.js
@@ -0,0 +1,20 @@
+// 1 - Affichage d'un text
+console.log("Hello Ramzy !");
+
+// 2 - Déclaration de variables
+let username = "Ramzy";
+let age = 25;
+let note = 19;
+
+// 3 - Affichage des variables
+console.log("Hello my name is ", username);
+console.log("My age is", age);
+
+// 4 - Lire des données depuis l'utilisateur
+let username2 = prompt("Enter your name");
+console.log("Hello", username2);
+
+// 5 - Type de données (variables)
+let x = 5; // Number
+let familyName = "Kemmoun"; // String
+let isStudent = true; // Boolean
