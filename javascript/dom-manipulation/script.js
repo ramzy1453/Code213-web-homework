c07773f7f7ac96bc8d8e660442cdbc6c9617f344
ramzy1453 <kemmounramzy93@gmail.com>
2025-01-31 17:56:34 +0100

    add events exemples

--- javascript/dom-manipulation/script.js
+++ javascript/dom-manipulation/script.js
@@ -1,14 +1,12 @@
 // 1 - Acceding to HTML element and modify them
 
-// 1 - 1 Get Element By Id
+// 1.1 Get Element By Id
 let usernameElement = document.getElementById("username");
 
 usernameElement.textContent = "Racim";
 
 // Get Element by Class
 let inputs = document.getElementsByClassName("number-input");
-
-console.log(inputs[0]);
 
 // Get Element by Tag Name
 
@@ -20,9 +18,8 @@ paragraphs[1].textContent = "Alaykom";
 // Get Element by Query Selector
 
 let spanElement = document.querySelector("body > h1 > span");
-console.log(spanElement);
 
-// 2 - Accessing to the inner HTML code
+// 1.2 - Accessing to the inner HTML code
 
 let ul = document.querySelector("ul");
 
@@ -34,14 +31,14 @@ ul.innerHTML += `
 
 `;
 
-// 3 - Modify HTML Attributes
+// 1.3 - Modify HTML Attributes
 inputs[0].setAttribute("type", "date");
 
 let image = document.querySelector("img");
 
 image.setAttribute("width", "300");
 
-// 4 - Modify CSS Properties
+// 1.4 - Modify CSS Properties
 // spanElement.style.color = "blue";
 // spanElement.style.backgroundColor = "red";
 // spanElement.style.border = "2px solid black";
@@ -52,4 +49,40 @@ spanElement.classList.add("myClass"); // add a new class
 
 spanElement.classList.remove("myClass"); // remove a class
 
-// 5 - Creating and removing HTML Element
+// 1.5 - removing HTML Element
+image.remove();
+
+// 2 - Events (click button, hover on mouse, press keyboard, scroll ext...)
+
+let btnAdd = document.querySelector(".add");
+console.log(btnAdd);
+
+// Add Event Listener to a button when you click on it
+
+usernameElement.addEventListener("click", function whenClick() {
+  usernameElement.textContent = "Ahmed";
+});
+
+// I want to add a dark mode when I hover on the button
+document.body.classList.add("light-mode");
+
+btnAdd.addEventListener("mouseover", function whenHover() {
+  document.body.classList.add("dark-mode");
+  document.body.classList.remove("light-mode");
+});
+
+btnAdd.addEventListener("mouseout", function whenHover() {
+  document.body.classList.add("light-mode");
+  document.body.classList.remove("dark-mode");
+});
+
+// When you click on add button, add a new list item
+btnAdd.addEventListener("click", function whenClick() {
+  ul.innerHTML += `<li>Test</li>`;
+});
+
+// When you click on remove button, remove the last list item
+let removeBtn = document.querySelector(".remove");
+removeBtn.addEventListener("click", function whenClick() {
+  ul.lastElementChild.remove();
+});
