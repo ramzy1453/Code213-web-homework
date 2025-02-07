16af267c5c047a1a2d6ed9db24b16c03879af64d
ramzy1453 <kemmounramzy93@gmail.com>
2025-02-07 16:33:31 +0100

    feat: create the first react project

---
+++ reactjs/react-first/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <p>RamzyWebsite</p>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+
+      <section>
+        <h1>Hello My name is ramzy</h1>
+        <p>I am a fullstack web developper</p>
+      </section>
+
+      <footer>
+        <div>Copyright Ramzy 2025</div>
+        <div>You can reach me on kemmounramzy93@gmail.com</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
