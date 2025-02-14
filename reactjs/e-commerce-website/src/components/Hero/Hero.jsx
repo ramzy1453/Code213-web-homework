721bac0cc5fc85f9909213bc3a18a8465daf4fd8
ramzy1453 <kemmounramzy93@gmail.com>
2025-02-14 12:28:46 +0100

    feat: add set state for choes selection

--- reactjs/e-commerce-website/src/components/Hero/Hero.jsx
+++ reactjs/e-commerce-website/src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./Hero.css";
 
 export default function Hero() {
+  const [actualShoe, setActualShoe] = useState(
+    "https://converse-store.vercel.app/assets/big-shoe1-VFPhAFde.png"
+  );
   return (
     <section id="hero">
       <div className="left-hero">
@@ -15,27 +19,44 @@ export default function Hero() {
         <button>Shop Now</button>
       </div>
       <div className="right-hero">
-        <img
-          src="https://converse-store.vercel.app/assets/big-shoe1-VFPhAFde.png"
-          width={500}
-          alt=""
-        />
+        <img src={actualShoe} width={500} alt="" />
         <div className="shoes-container">
-          <div className="shoe-item">
+          <div
+            className="shoe-item"
+            onClick={function () {
+              setActualShoe(
+                "https://converse-store.vercel.app/assets/big-shoe2-DQX3Rp_U.png"
+              );
+            }}
+          >
             <img
               alt=""
               src="https://converse-store.vercel.app/assets/big-shoe2-DQX3Rp_U.png"
               width={200}
             />
           </div>
-          <div className="shoe-item">
+          <div
+            className="shoe-item"
+            onClick={function () {
+              setActualShoe(
+                "https://converse-store.vercel.app/assets/big-shoe3-ncxL7lz9.png"
+              );
+            }}
+          >
             <img
               alt=""
               src="https://converse-store.vercel.app/assets/big-shoe3-ncxL7lz9.png"
               width={200}
             />
           </div>
-          <div className="shoe-item">
+          <div
+            className="shoe-item"
+            onClick={function () {
+              setActualShoe(
+                "https://converse-store.vercel.app/assets/big-shoe1-VFPhAFde.png"
+              );
+            }}
+          >
             <img
               alt=""
               src="https://converse-store.vercel.app/assets/big-shoe1-VFPhAFde.png"
